4bba1ec678158013a952cf9a2580c6ad633e16e8
atal-7 <atalkiba5@gmaill.com>
2023-12-25 03:53:26 +0530

    GetCurrentWeather and GetCities method to get current condition and cities from AccuWeather api

--- ForecastNow/Management/Helper/AccuWeatherHelper.cs
+++ ForecastNow/Management/Helper/AccuWeatherHelper.cs
@@ -1,5 +1,7 @@
 ﻿using ForecastNow.Model;
+using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -15,19 +17,43 @@ namespace ForecastNow.Management.Helper
         public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
         public const string API_KEY = "qG3yZv1VJE7f0IVwxQAcOXFhOMvHpDGE";
 
-        public static async List<City> GetCities(string query)
+        public static async Task<List<City>> GetCities(string query)
         {
-            List<City> cities=new List<City>();
 
+            List<City>? cities = null;
             string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
 
-            using(HttpClient client = new HttpClient())
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    cities = JsonConvert.DeserializeObject<List<City>>(json);
+                }
+            }
+            catch (Exception ex) { }
+
+            return cities ?? new List<City>();
+        }
+
+        public static async Task<CurrentWeather> GetCurrentWeather(string cityKey)
+        {
+            CurrentWeather? currentWeather = null;
+
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
             {
                 var response = await client.GetAsync(url);
-                string json= await response.Content.ReadAsStringAsync();
+                string json = await response.Content.ReadAsStringAsync();
+
+                var weatherlist = JsonConvert.DeserializeObject<List<CurrentWeather>>(json);
+                currentWeather = weatherlist?.FirstOrDefault();
             }
 
-            return cities;
+            return currentWeather ?? new CurrentWeather();
         }
     }
 }
